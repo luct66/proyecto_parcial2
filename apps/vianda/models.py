ea715be53f4b6d4726701c8aa4e4b26d37959524
“Oscar <lucmaxzarate@gmail.com>
2022-11-14 20:51:06 -0300

    correciones 1

--- apps/vianda/models.py
+++ apps/vianda/models.py
@@ -8,11 +8,13 @@ from secrets import choice
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Tipo_Plato(models.Model):
-    descripcion = models.CharField(max_length=200, blank= True)
+    descripcion = models.CharField(max_length=200)
     vigencia = models.BooleanField(default=True)
 
 
@@ -38,12 +40,13 @@ class Vianda(models.Model):
         ('Cancelado','cancelado'),
 
     )
-    frecuencia = models.CharField(choices=frecuencias,max_length=200, blank= True)
-    tipo_menu = models.CharField(choices=tiposmenus,max_length=200, blank= True)
-    fecha_inicio_vianda = models.DateField(blank = True)
-    cantidad = models.IntegerField(default=1)
-    estado = models.CharField(choices=estados,max_length=200,default='Pendiente' ,blank= True)
-    tipo_platos = models.ForeignKey(Tipo_Plato, on_delete=models.CASCADE,null=False)
+    frecuencia = models.CharField(choices=frecuencias,max_length=200)
+    tipo_menu = models.CharField(choices=tiposmenus,max_length=200)
+    fecha_inicio_vianda = models.DateField()
+    cantidad = models.IntegerField(default=1,
+        validators=[MaxValueValidator(100), MinValueValidator(1)])
+    estado = models.CharField(choices=estados,max_length=200,default='Pendiente')
+    tipo_platos = models.ManyToManyField(Tipo_Plato)
     user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
 
 
